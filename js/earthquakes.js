feae51f696179d35112e62c5db095d1d73333dcd
pdb119 <bradypat@uw.edu>
2013-11-10 23:47:04 -0800

    Finished

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -20,9 +20,45 @@ gov.usgs.quakesUrl = 'https://soda.demo.socrata.com/resource/earthquakes.json?$$
 
 //current earthquake dataset (array of objects, each representing an earthquake)
 gov.usgs.quakes;
-
 //reference to our google map
 gov.usgs.quakesMap;
+
+window.onload = function() {
+	document.querySelector(".message").innerHTML = "Loading... <img src='img/loading.gif'>";
+	gov.usgs.quakesMap = new google.maps.Map(document.querySelector(".map-container"), {center: new google.maps.LatLng(0,0), zoom: 2, mapTypeId: google.maps.MapTypeId.TERRAIN, streetViewControl: false});
+	$.getJSON(gov.usgs.quakesUrl, function (returnData) {
+		gov.usgs.quakes = returnData;
+		document.querySelector(".message").innerHTML = "Displaying " + gov.usgs.quakes.length + " earthquakes";		
+		mapQuakes(gov.usgs.quakes, gov.usgs.quakesMap);
+	});
+};
+
+function mapQuakes(data, map){
+	var i;
+	var I;
+	var newMarker;
+	for(i=0;i<data.length;i++){
+		I = data[i];
+		if(I.location.latitude && I.location.longitude){
+			//save index location as title so the correct information can be loaded later
+			newMarker = new google.maps.Marker({map: map, position: new google.maps.LatLng(I.location.latitude, I.location.longitude), title: i.toString()});
+			google.maps.event.addListener(newMarker, 'click', function () {								
+					var j = parseInt(this.getTitle());					
+					if(gov.usgs.iw){
+						gov.usgs.iw.close();
+					}
+					var newInfo = new google.maps.InfoWindow({content: new Date(gov.usgs.quakes[j].datetime).toLocaleString() + ': magnitude ' + gov.usgs.quakes[j].magnitude + ' at depth of ' + gov.usgs.quakes[j].depth + ' meters'});
+					gov.usgs.iw = newInfo;
+					gov.usgs.iw.open(gov.usgs.quakesMap, this);				
+
+			});
+		}
+	}
+	
+}
+
+
+
 
 //AJAX Error event handler
 //just alerts the user of the error
